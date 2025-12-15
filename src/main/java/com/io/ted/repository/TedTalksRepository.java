a8556a042c2e0e1efcfa44b0a22a256ff9979832
lote89 <42542694+lote89@users.noreply.github.com>
2025-12-15 10:35:38 +0100

    Update TedTalksRepository.java

--- src/main/java/com/io/ted/repository/TedTalksRepository.java
+++ src/main/java/com/io/ted/repository/TedTalksRepository.java
@@ -2,5 +2,11 @@ package com.io.ted.repository;
 
 import com.io.ted.model.TedTalk;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public interface TedTalksRepository extends JpaRepository<TedTalk, Long> {}
+@Repository
+public interface TedTalksRepository extends JpaRepository<TedTalk, Long> {
+  List<TedTalk> findBySpeaker(String speaker);
+  List<TedTalk> findByYear(Integer year);
+}
