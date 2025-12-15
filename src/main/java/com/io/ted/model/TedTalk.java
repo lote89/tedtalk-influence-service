858c71c052e3fac130448a136ab1db2dadea9238
lote89 <42542694+lote89@users.noreply.github.com>
2025-12-15 13:59:14 +0100

    Update TedTalk.java

--- src/main/java/com/io/ted/model/TedTalk.java
+++ src/main/java/com/io/ted/model/TedTalk.java
@@ -14,23 +14,55 @@ public class TedTalk {
     private String speaker;
     private Long views;
     private Long likes;
+
+   
     private LocalDate date;
     private String link;
 
+   
+    private Integer year;
+    private String url;
+
     public TedTalk() {}
 
+    public TedTalk(String title, String speaker, Integer year, Long views, Long likes, String url) {
+        this.title = title;
+        this.speaker = speaker;
+        this.year = year;
+        this.views = views;
+        this.likes = likes;
+        this.url = url;
+    }
     public Long getId() { return id; }
     public String getTitle() { return title; }
     public String getSpeaker() { return speaker; }
     public Long getViews() { return views; }
     public Long getLikes() { return likes; }
+
     public LocalDate getDate() { return date; }
     public String getLink() { return link; }
 
+    public Integer getYear() { return year; }
+    public String getUrl() { return url; }
+
+    
     public void setTitle(String title) { this.title = title; }
     public void setSpeaker(String speaker) { this.speaker = speaker; }
     public void setViews(Long views) { this.views = views; }
     public void setLikes(Long likes) { this.likes = likes; }
-    public void setDate(LocalDate date) { this.date = date; }
-    public void setLink(String link) { this.link = link; }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+        if (date != null) {
+            this.year = date.getYear(); // keep year in sync
+        }
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+        this.url = link; 
+    }
+
+    public void setYear(Integer year) { this.year = year; }
+    public void setUrl(String url) { this.url = url; }
 }
